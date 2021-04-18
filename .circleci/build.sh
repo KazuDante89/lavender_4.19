31ceb873fa5a4e20d5a929f3bf503237d18dbaff
KazuDante89 <karlemmanueldorvil@gmail.com>
2021-04-17 20:44:31 -0400

    Maybe the clang was the issue ? 	modified:   .circleci/build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/Klozz/Yuki-clang clang
 git clone --depth=1 https://github.com/KazuDante89/AnyKernel3-EAS -b lavender2 AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
@@ -43,10 +43,14 @@ function finerr() {
 function compile() {
     make O=out ARCH=arm64 Yuki_defconfig
     make -j$(nproc --all) O=out \
-                          ARCH=arm64 \
-			                    CC=clang \
-			                    CROSS_COMPILE=aarch64-linux-gnu- \
-			                    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+                       ARCH=arm64 \
+                         LD=ld.lld \
+                         NM=llvm-nm \
+                         AR=llvm-ar \
+                         CC="ccache clang" \
+               CLANG_TRIPLE=aarch64-linux-gnu- \
+              CROSS_COMPILE=aarch64-linux-gnu- \
+       CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
 
     if ! [ -a "$IMAGE" ]; then
         finerr
